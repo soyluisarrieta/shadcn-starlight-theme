74b089fc2663cc48b3c7e17e9f8ce4e76bf060ef
soyluisarrieta <luisarrieta796@gmail.com>
2025-03-31 17:29:33 -0500

    docs: update expressive code theme

--- packages/shadcn-starlight-theme/index.ts
+++ packages/shadcn-starlight-theme/index.ts
@@ -12,7 +12,35 @@ export default function shadcnStarlightTheme(): StarlightPlugin {
               ? false
               : {
                   ...(typeof config.expressiveCode === 'object' ? config.expressiveCode : {}),
-                  themes: ['vitesse-dark', 'vitesse-light'],
+                  styleOverrides: {
+                    codeBackground: 'var(--block-code-background)',
+                    borderColor: 'var(--block-code-background)',
+                    borderRadius: '0.75rem',
+                    textMarkers: {
+                      markBackground: 'var(--block-code-highlight)',
+                      markBorderColor: 'transparent',
+                      backgroundOpacity: '0.4',
+                      delBorderColor: 'transparent',
+                      insBorderColor: 'transparent'
+                    },
+                    frames: {
+                      editorBackground: 'var(--block-code-background)',
+                      editorTabBarBackground: 'var(--block-code-background)',
+                      editorTabBarBorderBottomColor: 'var(--border)',
+                      editorTabBarBorderColor: 'var(--block-code-background)',
+                      editorActiveTabBackground: 'var(--block-code-background)',
+                      editorActiveTabBorderColor: 'var(--block-code-background)',
+                      editorActiveTabIndicatorTopColor: 'var(--primary)',
+                      editorActiveTabIndicatorBottomColor: 'var(--border)',
+                      tooltipSuccessBackground: 'var(--input)',
+                      tooltipSuccessForeground: 'var(--input-foreground)',
+                      frameBoxShadowCssValue: 'unset',
+                      terminalBackground: 'var(--block-code-background)',
+                      terminalTitlebarBackground: 'var(--block-code-background)',
+                      terminalTitlebarBorderBottomColor: 'var(--border)',
+                    },
+                  },
+                  themes: ['github-dark-default', 'github-light'],
                 },
         })
       },
