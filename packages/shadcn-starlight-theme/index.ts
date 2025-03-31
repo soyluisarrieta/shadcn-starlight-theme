b9539e790261059a06b75bff4cd930407126b6d2
soyluisarrieta <luisarrieta796@gmail.com>
2025-03-30 19:06:37 -0500

    feat: setup theme

--- packages/shadcn-starlight-theme/index.ts
+++ packages/shadcn-starlight-theme/index.ts
@@ -4,17 +4,17 @@ export default function shadcnStarlightTheme(): StarlightPlugin {
   return {
     name: 'shadcn-starlight-theme',
     hooks: {
-      'config:setup'({ logger }) {
-        /**
-         * This is the entry point of your Starlight plugin.
-         * The `setup` hook is called when Starlight is initialized (during the Astro `astro:config:setup` integration
-         * hook).
-         * To learn more about the Starlight plugin API and all available options in this hook, check the Starlight
-         * plugins reference.
-         *
-         * @see https://starlight.astro.build/reference/plugins/
-         */
-        logger.info('Hello from the shadcn-starlight-theme plugin!')
+      'config:setup'({ updateConfig, config }) {
+        updateConfig({
+          customCss: [...(config.customCss ?? []), 'shadcn-starlight-theme/styles'],
+          expressiveCode:
+            config.expressiveCode === false
+              ? false
+              : {
+                  ...(typeof config.expressiveCode === 'object' ? config.expressiveCode : {}),
+                  themes: ['vitesse-dark', 'vitesse-light'],
+                },
+        })
       },
     },
   }
